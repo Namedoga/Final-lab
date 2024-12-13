d367e89c1f4af90ab923de6f77bad90c6aa3f91a
Othniel Powell <othniel@live.ca>
2024-12-13 18:19:53 -0500

    Added toString pt2

--- scripts.js
+++ scripts.js
@@ -15,16 +15,21 @@ toString() {
 } 
   class PerishableProductProperties extends ProductProperties {
     constructor(name, price, quantity, expirationDate) {
-     
+    super(name,price,quantity )
+        this.expirationDate = expirationDate;
+      }   
   
+      toString() {
+        return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+      }
+    }
+    
 
 
-}
 
 
 
-}
 
 
 
-}
+
