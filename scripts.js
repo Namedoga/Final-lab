d3d7fd2c063513485635b081ca48a41563101b1c
Othniel Powell <othniel@live.ca>
2024-12-13 19:32:45 -0500

    added took aways class product

--- scripts.js
+++ scripts.js
@@ -64,18 +64,6 @@ class Store {
   }
 }
 
-class Product {
-  constructor(name, price, quantity) {
-      this.name = name;
-      this.price = price;
-      this.quantity = quantity;
-  }
-
-  getTotalValue() {
-      return this.price * this.quantity;
-  }
-}
-
 
 
     
